0f06541f8c54978325ff3a9f5cea6559acb4f376
Alexander Seah <alexander.seah.min.rui@gmail.com>
2026-01-30 20:32:28 +0800

    init

---
+++ exercises/01_variables/variables1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: Add the missing keyword.
+    mut x = 5;
+
+    println!("x has the value {x}");
+}
---
+++ exercises/01_variables/variables6.rs
@@ -0,0 +1,6 @@
+// TODO: Change the line below to fix the compiler error.
+mut NUMBER = 3;
+
+fn main() {
+    println!("Number: {NUMBER}");
+}
