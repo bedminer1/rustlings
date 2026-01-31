729750f88666b1296aef2911f31c740275c1699b
Alexander Seah <alexander.seah.min.rui@gmail.com>
2026-01-31 22:30:45 +0800

    done with exercise 10, modules

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -15,7 +15,11 @@ mod delicious_snacks {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
+
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 }
+
 
 fn main() {
     println!(
